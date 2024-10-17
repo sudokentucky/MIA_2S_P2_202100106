99edf05b8c8afc1345e7583dd8c7b794fe221472
KENETH WILLARD LĂ“PEZ OVALLE <3116549240704@ingenieria.usac.edu.gt>
2024-10-16 21:57:08 -0600

    Agregar formulario para visualizar archivos

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,30 +2,24 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import CommandExecution from "./CommandExecution";
 import Login from "./Login";
 import UserManagement from "./UserManagement";
+import FileVisualizer from "./FileVisualizer"; // Importa el componente de visualización de archivos
 import Navbar from "./NavBar";
 import { useState } from "react";
 
 function App() {
-  const [navbarUpdated, setNavbarUpdated] = useState(false); // Estado para actualizar el Navbar
-
-  // Función que se pasa a CommandExecution para forzar la actualización
-  const updateNavbar = () => {
-    setNavbarUpdated((prev) => !prev); // Cambia el estado para forzar el renderizado
-  };
+  const [navbarUpdated, setNavbarUpdated] = useState(false);
 
   return (
     <Router>
       <div className="min-h-screen flex flex-col">
-        {/* Incluimos el Navbar, que se renderizará cada vez que navbarUpdated cambie */}
         <Navbar navbarUpdated={navbarUpdated} />
-        
-        {/* Rutas principales */}
         <div className="flex-grow p-4" style={{ backgroundColor: "#E6E6FA" }}>
           <div className="container mx-auto">
             <Routes>
-              <Route path="/" element={<CommandExecution updateNavbar={updateNavbar} />} />
+              <Route path="/" element={<CommandExecution updateNavbar={() => setNavbarUpdated(!navbarUpdated)} />} />
               <Route path="/login" element={<Login />} />
               <Route path="/user-management" element={<UserManagement />} />
+              <Route path="/file-visualizer" element={<FileVisualizer />} /> {/* Nueva ruta */}
             </Routes>
           </div>
         </div>
